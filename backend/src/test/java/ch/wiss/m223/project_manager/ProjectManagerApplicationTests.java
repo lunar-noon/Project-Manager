7045cec36346a9cfad11394e2456ddf68e1b71b7
天ㅊ Noon OωO ㅊ天 <78344868+lunar-noon@users.noreply.github.com>
2025-05-22 10:51:35 +0200

    added backend

---
+++ backend/src/test/java/ch/wiss/m223/project_manager/ProjectManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.wiss.m223.project_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
